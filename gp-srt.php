45e0f12146ca825a64256c52bfc8f9eddec23a27
Greg Ross <greg@toolstack.com>
2016-07-05 18:46:06 -0400

    Add support for sort by type in GP 2.1.

--- gp-srt.php
+++ gp-srt.php
@@ -12,8 +12,18 @@ License: GPLv2 or later
 
 // Add an action to WordPress's init hook to setup the plugin.  Don't just setup the plugin here as the GlotPress plugin may not have loaded yet.
 add_action( 'gp_init', 'gp_srt_init' );
+add_filter( 'gp_sort_by_fields', 'gp_srt_sort_by_fields' );
 
 // This function creates the plugin.
 function gp_srt_init() {
 	include( dirname( __FILE__ ) . '/format_srt.php' );
+}
+
+function gp_srt_sort_by_fields( $sort_fields ) {
+	$sort_fields['context'] = array( 
+		'title' => __( 'Context', 'glotpress' ),
+		'sql_sort_by' => 'o.context %s',
+	);
+	
+	return $sort_fields;
 }
